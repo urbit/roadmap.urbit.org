761c2c2653d06051ce1cfe5a0717d2bad8f513d5
Matilde Park <m@park.computer>
2022-11-02 17:01:28 -0700

    grid: allow class passing

--- components/Grid.js
+++ components/Grid.js
@@ -1,5 +1,5 @@
-export default function Grid({ children }) {
-    return <div className="grid grid-cols-12">
+export default function Grid({ children, className = "" }) {
+    return <div className={"w-full grid grid-cols-12 " + className}>
         {children}
     </div>
 }
