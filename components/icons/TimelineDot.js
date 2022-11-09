30fbd771dc8946508c7675012f5b496fc98f9da9
Matilde Park <m@park.computer>
2022-11-08 21:42:43 -0800

    TimelineDot: fix react issue

--- components/icons/TimelineDot.js
+++ components/icons/TimelineDot.js
@@ -1,6 +1,6 @@
 export default function TimelineDot({ className = "" }) {
     return <svg width="18" height="18" viewBox="0 0 18 18" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="9" cy="9" r="8" className="fill-white stroke-black" stroke-width="2" />
+        <circle cx="9" cy="9" r="8" className="fill-white stroke-black" strokeWidth="2" />
         <circle cx="9" cy="9" r="4" className="fill-black" />
     </svg>
 }
