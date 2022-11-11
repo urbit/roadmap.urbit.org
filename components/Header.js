5b29703626d42dc70a6de7740c3e5c30f31bc8a9
Matilde Park <m@park.computer>
2022-11-11 14:29:35 -0800

    header: stagger on weird 768px viewport

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ export default function Header({ title }) {
         <>
             <h1 className="md:hidden pb-10">~</h1>
             <h2 className="text-3xl font-bold sig col-start-1 md:col-start-2 col-end-4">Roadmap</h2>
-            <h2 className="text-3xl col-start-1 md:col-start-4 col-span-8 mt-4 md:mt-0 font-normal">{title}</h2>
+            <h2 className="text-3xl col-start-1 md:col-start-2 lg:col-start-4 col-span-8 mt-4 md:mt-0 font-normal">{title}</h2>
         </>
     )
 }
