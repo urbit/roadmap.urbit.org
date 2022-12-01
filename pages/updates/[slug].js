990e45b883be3b1ea8842d0f9ec78e59d05e8c13
Gordon <gordon@hey.com>
2022-12-01 14:56:15 -0800

    back link to updates, agents in clay pr

--- pages/updates/[slug].js
+++ pages/updates/[slug].js
@@ -25,6 +25,7 @@ export default function UpdatePost({
   const router = useRouter();
   const date = generateDisplayDate(post.date);
 
+
   return (
     <BasicPage
       sectionTitle="Update"
@@ -35,26 +36,14 @@ export default function UpdatePost({
     >
       <Grid className="col-span-full md:mt-40 relative">
         {/* Sidebar */}
-        <div className="col-start-2 col-end-4">
-          <Sidebar search={search}>
-            {dirs.map((dir) => {
-              return <li key={dir.title}>
-                <Link href={dir.link}>
-                  <a className={cn("font-light text-base md:text-xl", {
-                    "text-wall-400": dir.link !== router.pathname,
-                    "": dir.link === router.pathname
-                  })}>{dir.title}</a>
-                </Link>
-              </li>
-            })}
-            <Link href="/updates">
-                <a className={cn("text-base md:text-xl", {
-                    "text-wall-400": 'updates' !== router.pathname,
-                    "": 'updates' === router.pathname
-                })}>Updates</a>
-            </Link>
-          </Sidebar>
-        </div>
+
+          <div className="col-start-2 col-end-4">
+              <Sidebar search={search}>
+                  <li><Link href={`/updates`}><a className="text-lg text-green-400 back">{"<-"} Back</a></Link></li>
+              </Sidebar>
+          </div>
+
+
         {/* Content */}
         <div className="col-span-full md:col-start-4 md:col-end-12 lg:col-end-10 markdown mt-16 md:mt-0">
 
